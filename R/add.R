59ab5d71748bbcb86dc051ecda500cf90eaba869
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2020-01-22 22:54:51 -0800

    add add.R

---
+++ R/add.R
@@ -0,0 +1,12 @@
+#' Add two numbers
+#'
+#' @param x The first number
+#' @param y The second number
+#' @return The sum of x and y
+#' @export
+#' @examples
+#' add(1, 2)
+#' add(2, 3)
+add <- function(x, y) {
+  x + y
+}
